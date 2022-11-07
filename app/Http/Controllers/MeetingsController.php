e34687d2dcc2f94c56a27d24494a50bae7310c78
nedicom <m6132@yandex.ru>
2022-11-07 23:10:25 +0300

    Создано представление заседаний и фильтр по дням, неделям и месяцам

--- app/Http/Controllers/MeetingsController.php
+++ app/Http/Controllers/MeetingsController.php
@@ -2,16 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Http\Requests\MeetingsRequest;
-use App\Http\Requests\UpdateMeetingsRequest;
+//use App\Http\Requests\UpdateMeetingsRequest;
 use App\Models\Meetings;
 use App\Models\User;
+use Carbon\Carbon;
 
 class MeetingsController extends Controller
 {
-    public function index()
-    {
-     return view ('meetings/meetings', ['data' => Meetings::all()], ['datalawyers' =>  User::all()]);
+    public function index(Request $request){
+
+        $calendar = $request->input('calendar');//month, year, day
+        $checkedlawyer = $request->input('checkedlawyer');//lawyer
+
+        if($calendar == 'week'){
+          return view ('meetings/meetings', ['data' => Meetings::select("*")
+          ->whereBetween('date', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+          ->where('lawyer', '=', $checkedlawyer)
+          ->get()],
+          ['datalawyers' =>  User::all()]);
+        }
+        elseif($calendar == 'day'){
+          return view ('meetings/meetings', ['data' => Meetings::select("*")
+          ->whereBetween('date', [Carbon::now()->startOfDay(), Carbon::now()->endOfDay()])
+          ->where('lawyer', '=', $checkedlawyer)
+          ->get()],
+          ['datalawyers' =>  User::all()]);
+        }
+        elseif($calendar == 'month'){
+          return view ('meetings/meetings', ['data' => Meetings::select("*")
+          ->whereBetween('date', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])
+          ->where('lawyer', '=', $checkedlawyer)
+          ->get()],
+          ['datalawyers' =>  User::all()]);
+        }
+        elseif(!empty($checkedlawyer)){
+          return view ('meetings/meetings', ['data' => Meetings::select("*")
+          ->where('lawyer', '=', $checkedlawyer)
+          ->get()],
+          ['datalawyers' =>  User::all()]);
+        }
+        else{
+          return view ('meetings/meetings', ['data' => Meetings::All()],
+          ['datalawyers' =>  User::all()]);
+        }
+
+
     }
 
     public function create(MeetingsRequest $req)
--- resources/views/meetings/meetings.blade.php
+++ resources/views/meetings/meetings.blade.php
@@ -5,16 +5,60 @@
 
 
 @section('title')
-  Заседания/встречи
+  Заседания
 @endsection
 
 @section('leftmenuone')
   <li class="nav-item text-center p-3">
-    <a class="text-white text-decoration-none" href="#" data-bs-toggle="modal" data-bs-target="#meetingModal">Добавить встречу</a>
+    <a class="text-white text-decoration-none" href="#" data-bs-toggle="modal" data-bs-target="#meetingModal">Добавить заседание</a>
   </li>
 @endsection
 
 @section('main')
+
+{{-- start filter meetings--}}
+  <div class = "row p-4">
+      <form class = "row" action="" method="GET">
+
+        <div class="col-8 d-flex justify-content-evenly">
+          <div class="">
+            <input type="radio" class="btn-check" value="day" name="calendar" id="day"
+              @if (app('request')->input('calendar') == 'day') checked @endif
+              onchange="this.form.submit()">
+            <label class="btn btn-outline-success" for="day">День</label>
+
+            <input type="radio" class="btn-check" value="week" name="calendar" id="week"
+              @if (app('request')->input('calendar') == 'week') checked @endif
+              onchange="this.form.submit()">
+            <label class="btn btn-outline-success" for="week">Неделя</label>
+
+            <input type="radio" class="btn-check" value="month" name="calendar" id="month"
+              @if (app('request')->input('calendar') == 'month') checked @endif
+              onchange="this.form.submit()">
+            <label class="btn btn-outline-success" for="month">Месяц</label>
+            </div>
+
+            <div class="">
+              <select class="form-select" name="checkedlawyer" id="checkedlawyer">
+                    @foreach($datalawyers as $el)
+                      <option value="{{$el -> id}}" @if (($el -> id) == (app('request')->input('checkedlawyer'))) selected @endif>
+                        {{$el -> name}}
+                      </option>
+                    @endforeach
+              </select>
+            </div>
+
+            <div class="">
+              <button type="submit" class="btn btn-primary">Применить</button>
+              <a href='meetings' class='button btn btn-secondary'>Сбросить</a>
+            </div>
+
+        </div>
+
+      </form>
+  </div>
+  {{-- end filter meetings--}}
+
 
 
 {{-- start views for all meetings--}}
@@ -24,13 +68,15 @@
     <div class="col-3 my-3">
       <div class="card border-light">
           <div class="card-body">
-            <h5 class="card-title">{{$el -> name}}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{$el -> price}}</h6>
-              <h4 class="header-title mb-3"></h4>
-
-               <span class="badge bg-success"> +5.35% </span>
-
-              <p>{{$el -> created_at}}</p>
+            <h5 class="card-title">{{$el -> description}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{$el -> client}}</h6>
+              <h4 class="header-title mb-3">{{$el -> name}}</h4>
+               <span class="badge bg-success"> {{$el['date']}}</span>
+              <p>
+                @foreach($datalawyers as $ellawyer)
+                  @if ($ellawyer -> id == $el -> lawyer)  {{$ellawyer -> name}} @endif
+                @endforeach
+              </p>
 
 
               <div class="mt-3 row d-flex justify-content-center">
