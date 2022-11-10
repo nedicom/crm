8391a1b000e993aeb32404d43b9be4b7125a1763
nedicom <m6132@yandex.ru>
2022-11-10 10:00:56 +0300

    git test mith mchost

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.app')
+
+@section('head')
+@endsection
+
+@section('title')
+  Главная
+@endsection
+
+@section('leftmenuone')
+  <li class="nav-item text-center p-3">
+    <a class="text-white text-decoration-none" href="#" data-bs-toggle="modal" data-bs-target="#taskModal">ссылка меню</a>
+  </li>
+  <li class="nav-item text-center p-3">
+    <a class="text-white text-decoration-none" href="#" data-bs-toggle="modal" data-bs-target="#taskModal">еще одна ссылка</a>
+@endsection
+
+@section('main')
+тут будут представления и инструкция
+@endsection
