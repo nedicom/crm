53f956bab450d976b77b70aa7c2be07f6027c701
nedicom <m6132@yandex.ru>
2022-11-10 19:55:47 +0300

    user register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\LawyersController;
@@ -122,6 +124,23 @@ Route::POST('/getclient', [GetclientAJAXController::class, 'getclient'])->name('
 Route::get('/lawyers', [LawyersController::class, 'Alllawyers'])->name('lawyers');
 
 Route::post('/lawyers/add', [LawyersController::class, 'submit'])->name('add-lawyer');
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
 Auth::routes();
