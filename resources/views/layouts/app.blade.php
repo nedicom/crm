467ee7612fd9b6dbc0a7d2ac7c23b26ec304ecbb
Mark <m6132@yandex.ru>
2022-11-19 12:59:16 +0300

    style path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-      <link href="public/style.css" rel="stylesheet">
+      <link href="style.css" rel="stylesheet">
       @yield('head')
 
   </head>
