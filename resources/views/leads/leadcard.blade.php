5e2deb411fd59543c8a6d14acef0845faf5fb425
Mark <m6132@yandex.ru>
2022-11-19 16:54:01 +0300

    add fire icon on leads

---
+++ resources/views/leads/leadcard.blade.php
@@ -0,0 +1,44 @@
+  <script>
+    $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    })
+  </script>
+
+<div class="my-3 d-inline-block" style="width: 20rem;">
+          <div class="card border-light">
+              <div class="card-body">
+                <div class ="d-flex justify-content-between">
+                  <h6 class="card-title text-truncate">{{$el -> name}}</h6>
+                  <div>
+                    <span class="badge
+                    @if ($el -> status == 'поступил') badge-postupil
+                    @elseif ($el -> status == 'в работе') badge-vrabote
+                    @elseif ($el -> status == 'конвертирован') badge-convertirovan
+                    @else badge-udalen
+                    @endif">{{$el -> status}} </span>
+                  </div>
+                </div>
+
+               <h4 class="header-title mb-3">{{$el -> phone}}</h4>
+
+               <div class ="d-flex justify-content-between">
+                 <p class ="">{{$el -> created_at}}</p>
+                 <p class ="">{{$el -> source}}</p>
+               </div>
+
+
+
+               <div class="mt-1 d-flex justify-content-between">
+                <div class="col-4">
+                  <a class="btn btn-light w-100" href="{{ route ('showLeadById', $el->id) }}">
+                  <i class="bi-three-dots"></i></a>
+                </div>
+                <div class="">
+                @if($el -> action)<a class ="btn" data-toggle="tooltip" title="{{$el -> action}}">
+                   <i class="bi bi-fire" style="color: red"></i></a>
+                @endif
+                </div>
+              </div>
+                </div>
+              </div>
+          </div>
--- resources/views/leads/leads.blade.php
+++ resources/views/leads/leads.blade.php
@@ -21,42 +21,23 @@
 
   @include('inc/filter.leadfilter')
 
-      @foreach($data as $el)
-        <div class="col-3 my-3">
-          <div class="card border-light">
-              <div class="card-body">
-                <div class ="d-flex justify-content-between">
-                  <h6 class="card-title">{{$el -> name}}</h6>
-                  <div>
-                    <span class="badge
-                    @if ($el -> status == 'поступил') badge-postupil
-                    @elseif ($el -> status == 'в работе') badge-vrabote
-                    @elseif ($el -> status == 'конвертирован') badge-convertirovan
-                    @else badge-udalen
-                    @endif">{{$el -> status}} </span>
-                  </div>
-                </div>
+    <div class="row">
+      <div class="col-4">
+        @foreach($data as $el)
+          @if($el -> status == "поступил")
+            @include('leads/leadcard')
+          @endif
+        @endforeach
+      </div>
 
-               <h4 class="header-title mb-3">{{$el -> phone}}</h4>
-
-               <div class ="d-flex justify-content-between">
-                 <p class ="">{{$el -> created_at}}</p>
-                 <p class ="">{{$el -> source}}</p>
-               </div>
-
-               <p class="header-title mb-3 text-truncate">что делаем: {{$el -> action}}</p>
-
-               <div class="mt-3 row d-flex justify-content-center">
-                <div class="col-4 mb-3">
-                  <a class="btn btn-light w-100" href="{{ route ('showLeadById', $el->id) }}">
-                  <i class="bi-three-dots"></i></a>
-                </div>
-              </div>
-                </div>
-              </div>
-          </div>
-      @endforeach
-
+      <div class="col-8">
+        @foreach($data as $el)
+          @if($el -> status !== "поступил")
+            @include('leads/leadcard')
+          @endif
+        @endforeach
+      </div>
+    </div>
 
 {{-- end views for all services--}}
 
