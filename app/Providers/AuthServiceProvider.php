3f03c4f79fc6e04d5d09cea81ace04e6bf423835
Mark <m6132@yandex.ru>
2022-11-19 18:57:57 +0300

    user verification

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,9 +29,9 @@ class AuthServiceProvider extends ServiceProvider
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
         return (new MailMessage)
-            ->subject('Verify Email Address')
-            ->line('Click the button below to verify your email address.')
-            ->action('Verify Email Address', $url);
+            ->subject('Подтверждение адреса')
+            ->line('Чтобы подтвердить адрес почты нажмите на кнопку ниже')
+            ->action('Подтвердить адрес', $url);
     });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,21 @@ use App\Http\Controllers\MeetingsController;
 use App\Http\Controllers\SourceController;
 use App\Http\Controllers\GetclientAJAXController;
 
-Route::get('/', function () {
-    return redirect('/home');
-});
-Route::get('/logout', function () {
-    return redirect('/login');
-});
+  Route::get('/', function () {
+      return redirect('/home');
+  });
+  Route::get('/logout', function () {
+      return redirect('/login');
+  });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+  Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+
+  Route::get('/contacts', function () {return view('contacts');})->middleware('auth');
 
 
-Route::get('/contacts', function () {return view('contacts');})->middleware('auth');
 
 
-  Route::middleware(['auth'])->group(function () {
+  Route::middleware(['verified'])->group(function () {
 
     Route::controller(SourceController::class)->group(function () {
       Route::post('/source/add', 'addSource')->name('addSource');
@@ -59,37 +60,6 @@ Route::get('/contacts', function () {return view('contacts');})->middleware('aut
       Route::post('/tasks/{id}/delete', 'TaskDelete')->name('TaskDelete');
     });
 
-
-
-
-  /*    Route::get('/tasks', [TasksController::class, 'index'])->name('tasks')->middleware('auth');
-
-    Route::post('/tasks/add', [TasksController::class, 'create'])->name('addtask')->middleware('auth');
-
-    Route::get('/tasks/{id}', [TasksController::class, 'showTaskById'])->name('showTaskById')->middleware('auth');
-
-    Route::post('/tasks/{id}/edit', [TasksController::class, 'editTaskById'])->name('editTaskById')->middleware('auth');
-
-    Route::get('/tasks/{id}/delete', [TasksController::class, 'TaskDelete'])->name('TaskDelete')->middleware('auth');
-
-
-
-  Route::get('/clients', [ClientsController::class, 'AllClients'])->name('clients')->middleware('auth');
-
-    Route::post('/clients/add', [ClientsController::class, 'submit'])->name('add-client')->middleware('auth');
-
-    Route::get('/clients/{id}', [ClientsController::class, 'showClientById'])->name('showClientById')->middleware('auth');
-
-    Route::get('/clients/{id}/edit', [ClientsController::class, 'updateClient'])->name('Client-Update')->middleware('auth');
-
-    Route::post('/clients/{id}/edit', [ClientsController::class, 'updateClientSubmit'])->name('Client-Update-Submit')->middleware('auth');
-
-    Route::get('/clients/{id}/delete', [ClientsController::class, 'ClientDelete'])->name('Client-Delete')->middleware('auth');
-*/
-
-
-
-
     Route::controller(MeetingsController::class)->group(function () {
       Route::get('/meetings', 'index')->name('meetings');
       Route::post('/meetings/add', 'create')->name('addmeetings');
@@ -105,65 +75,47 @@ Route::get('/contacts', function () {return view('contacts');})->middleware('aut
       Route::post('/tasks/{id}/edit', 'editTaskById')->name('editTaskById');
       Route::get('/tasks/{id}/delete', 'TaskDelete')->name('TaskDelete');
     });
+
+    Route::get('/services', [ServicesController::class, 'showservices'])->name('showservices')->middleware('auth');
+    Route::post('/services/add', [ServicesController::class, 'addservice'])->name('addservice')->middleware('auth');
+
+    Route::controller(PaymentsController::class)->group(function () {
+      Route::get('/payments', 'showpayments')->name('payments');
+      Route::post('/payments/add', 'addpayment')->name('addpayment');
+      Route::get('/payments/{id}', 'showPaymentById')->name('showPaymentById');
+      Route::post('/payments/{id}/edit', 'PaymentUpdateSubmit')->name('PaymentUpdateSubmit');
+      Route::get('/payments/{id}/delete', 'PaymentDelete')->name('PaymentDelete');
+      });
+
+      Route::get('/lawyers', [LawyersController::class, 'Alllawyers'])->name('lawyers');
+      Route::post('/lawyers/add', [LawyersController::class, 'submit'])->name('add-lawyer');
   });
 
+//Route::get('/test', [MeetingsController::class, 'index'])->name('test');
 
 
+    Route::get('ajax',function() {
+       return view('message');
+    });
+
+    Route::POST('/getclient', [GetclientAJAXController::class, 'getclient'])->name('getclient')->middleware('auth');
 
 
+    Route::get('/email/verify', function () {
+        return view('auth/verify');
+    })->middleware('auth')->name('verification.notice');
 
-Route::get('/services', [ServicesController::class, 'showservices'])->name('showservices')->middleware('auth');
-
-Route::post('/services/add', [ServicesController::class, 'addservice'])->name('addservice')->middleware('auth');
-
-
-
-Route::get('/payments', [PaymentsController::class, 'showpayments'])->name('payments')->middleware('auth');
-
-Route::post('/payments/add', [PaymentsController::class, 'addpayment'])->name('addpayment')->middleware('auth');
-
-Route::get('/payments/{id}', [PaymentsController::class, 'showPaymentById'])->name('showPaymentById')->middleware('auth');
-
-//Route::get('/payments/{id}/edit', [PaymentsController::class, 'PaymentUpdate'])->name('PaymentUpdate')->middleware('auth');
-
-Route::post('/payments/{id}/edit', [PaymentsController::class, 'PaymentUpdateSubmit'])->name('PaymentUpdateSubmit')->middleware('auth');
-
-Route::get('/payments/{id}/delete', [PaymentsController::class, 'PaymentDelete'])->name('PaymentDelete')->middleware('auth');
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        return redirect('/home');
+    })->middleware(['auth', 'signed'])->name('verification.verify');
 
 
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
 
-Route::get('/test', [MeetingsController::class, 'index'])->name('test')->middleware('auth');
-
-
-Route::get('ajax',function() {
-   return view('message');
-});
+        return back()->with('message', 'Ссылка для верификации отправлена');
+    })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
-Route::POST('/getclient', [GetclientAJAXController::class, 'getclient'])->name('getclient')->middleware('auth');
-
-
-
-Route::get('/lawyers', [LawyersController::class, 'Alllawyers'])->name('lawyers');
-
-Route::post('/lawyers/add', [LawyersController::class, 'submit'])->name('add-lawyer');
-
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
-
-Auth::routes();
+  Auth::routes();
