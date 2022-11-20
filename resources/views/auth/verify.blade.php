08e34ea82492310b87db0d74d1b6305d0806cbc2
Mark <m6132@yandex.ru>
2022-11-20 15:07:06 +0300

    register form

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,22 +5,32 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Подтвердите электронную почту') }}</div>
+                <div class="card-header">{{ __('Необходимо подтвердить почту для доступа') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('Свежая ссылка на верификацию направлена') }}
+                            {{ __('Свежая ссылка на верификацию уже направлена') }}
                         </div>
                     @endif
 
-                    {{ __('Проверьте ссылку на верификацию (особенно папку спам).') }}
-                    {{ __('Если Вы ее не получили') }},
+                    {{ __('Нажмите на кнопку, затем проверьте почту.') }}
                     <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
                         @csrf
-                        <button type="submit" class="btn btn-primary p-0 m-0 align-baseline">{{ __('нажмите здесь ') }}</button>.
-                    {{ __('Не кликайте по 10 раз. Достаточно одного.') }},
+                        <button type="submit" id="clicked" class="btn btn-primary">{{ __('нажмите здесь ') }}</button>
+                        
+                    {{ __('Не кликайте по 10 раз. Достаточно одного.') }}
                     </form>
+                    @php
+                    $mail = Auth::user()->email;
+                    $explode = explode("@",$mail);                    
+                    @endphp
+                    <script>
+                        $( "#clicked" ).click(function() {
+                         $( "#mail" ).show( "fast" );
+                        });
+                    </script>
+                    <span id="mail" style="display:none;">проверьте: <a href="http://{{ $explode[1] }}" type="submit" class="btn btn-primary">{{ Auth::user()->email }}</a></span>
                 </div>
             </div>
         </div>
