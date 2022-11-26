edf503f96eda0c15f2ac3a343e2cf418b0deb3fa
Mark <m6132@yandex.ru>
2022-11-26 17:47:14 +0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
       <link href="public/style.css" rel="stylesheet">
-      <link rel="icon" href="favicon.svg" type="image/svg+xml"/>
+      <link rel="icon" href="https://crm.nedicom.ru/public/favicon.svg" type="image/svg+xml"/>
       
       @yield('head')
 
