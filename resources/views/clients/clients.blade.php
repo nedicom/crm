0a36a29258b597c71e3890163165400c5d2ce8ff
Mark <m6132@yandex.ru>
2022-12-15 17:27:58 +0300

    clients

--- resources/views/clients/clients.blade.php
+++ resources/views/clients/clients.blade.php
@@ -19,74 +19,80 @@
 
     @foreach($data as $el)
 
-      <div class= 'col-md-6 col-xxl-3 my-3'>
-        <div class= 'card border-light'>
-          <div class= 'd-inline-flex justify-content-between px-4 pt-2'>
+      <div class= 'card row my-3 p-3 border'>
+        <div class="text-center d-inline-flex justify-content-between align-items-center">
 
-        <div>
-          @foreach($el -> tasksFunc as $val)
-            @if($val -> lawyer == Auth::user()->id && $val -> status == 'просрочена')
-            <i class="bi bi-alarm" style = "color: red;"></i>          
-            @endif
-          @endforeach
-        </div>
-
-        <div>
-          @foreach($el -> tasksFunc as $val)
-            @if($val -> lawyer == Auth::user()->id && $val -> status == 'в работе')
-            <i class="bi bi-person-workspace" style = "color: green;"></i>          
-            @endif
-          @endforeach
-        </div>
-
-        <div>
-          @foreach($el -> tasksFunc as $val)
-            @if($val -> lawyer == Auth::user()->id && $val -> status == 'ожидает')
-            <i class="bi bi-hourglass-split" style = "color: orange;"></i>          
-            @endif
-          @endforeach
-        </div>
-
-        <div>
-            @if ($el -> status == 1)
-                <i class="bi bi-person" style = "color: #0acf97;"></i>
-            @else
-                <i class="bi bi-person text-secondary"></i>
-            @endif
+        <div class="col-4">
+            <div class="col-6 d-flex justify-content-center">
+                  <div class="px-1 col-4">
+                    <a class="btn btn-light w-100" href="{{ route ('showClientById', $el->id) }}">
+                    <i class="bi-three-dots"></i></a>
+                  </div>
+                  <div class="px-1 col-4">
+                    <a class="btn btn-light w-100 nameToForm" href="#"
+                    dataclient="{{$el -> name}}" datavalueid="{{$el -> id}}" data-bs-toggle="modal" data-bs-target="#taskModal">
+                    <i class="bi-clipboard-plus"></i></a>
+                  </div>
+                  <div class="px-1 col-4">
+                  <img src="@if($el -> userFunc -> avatar){{$el -> userFunc -> avatar}}@endif" style="width: 30px;  height:30px"
+                  class="rounded-circle" data-bs-toggle="tooltip" data-bs-placement="top" 
+                  title="@if($el -> userFunc -> name){{$el -> userFunc -> name}}@endif">
+                </div>
             </div>
-
           </div>
+            <div class="col-4 text-muted text-truncate fw-bold">{{$el -> name}}</div>
 
-          <div class="text-center">
-            <h6 class="mb-2 px-3 text-muted text-truncate">{{$el -> name}}</h6>
-            <div class="w-100 d-inline-flex justify-content-between align-items-center px-5">
+          <div class="col-4 d-flex align-items-center justify-content-end">
+            <div class="col-6 d-flex align-items-cente justify-content-end">
               <div>
                 <p class="mb-0 text-muted">{{$el -> phone}}</p>
                 <p class="mb-0 text-muted">{{$el -> email}}</p>
               </div>
-              <div>
-              </div>
-              <div>
-                <img src="@if($el -> userFunc -> avatar){{$el -> userFunc -> avatar}}@endif" style="width: 30px;  height:30px"
-                class="rounded-circle" data-bs-toggle="tooltip" data-bs-placement="top" 
-                title="@if($el -> userFunc -> name){{$el -> userFunc -> name}}@endif">
-              </div>
             </div>
-            <hr class="bg-dark-lighten my-3">         
-            <div class="mt-3 row d-flex justify-content-center">
-                <div class="col-4 mb-3">
-                  <a class="btn btn-light w-100" href="{{ route ('showClientById', $el->id) }}">
-                  <i class="bi-three-dots"></i></a>
-                </div>
-                <div class="col-4 mb-3">
-                  <a class="btn btn-light w-100 nameToForm" href="#"
-                  dataclient="{{$el -> name}}" datavalueid="{{$el -> id}}" data-bs-toggle="modal" data-bs-target="#taskModal">
-                  <i class="bi-clipboard-plus"></i></a>
-                </div>
+          
+
+            <div class="col-6 d-flex align-items-center justify-content-end">
+              <div  class="px-3">
+                @if ($el -> status == 1)
+                    <i class="bi bi-person" style = "font-size: 2rem; color: #0acf97;"></i>
+                @else
+                    <i class="bi bi-person" style = "font-size: 2rem; color: gray;"></i>
+                @endif
+              </div>
             </div>
           </div>
-
         </div>
+
+        <div class="row">
+          <div class="d-flex flex-wrap">            
+            @foreach($el -> tasksFunc as $val)
+              @if($val -> status !== 'выполнена')
+              <div class="col-2 px-3 py-4 border border-4 border-light rounded" style="background-color:
+                @if($val -> status == 'просрочена') LightCoral
+                @elseif($val -> status == 'в работе') MediumAquaMarine
+                @elseif($val -> status == 'ожидает') Cornsilk
+                @else Cornsilk  @endif
+                
+              ;">
+                  <span class="px-1 fw-normal bg-white border border-white rounded-top" style="font-size: 14px;!important">{{$val -> date['value']}}</span>   
+                  <div class="px-1 fw-normal bg-white border border-white rounded-end"  style="height: 80px; overflow: hidden; position: relative;"> 
+                    <a href="/tasks/{{$val -> id}}" style="font-size: 14px;!important" target="_blank">{{$val -> name}}</a>
+                  </div>                                         
+              </div>                       
+              @endif
+            @endforeach
+            <div class="col-2 px-3 py-4 border border-4 border-light rounded" style="background-color: Cornsilk;;">
+                  <span class="px-1 fw-normal bg-white border border-white rounded-top" style="font-size: 14px;!important">добавить задачу</span>   
+                  <div class="px-1 fw-normal bg-white border border-white rounded-end d-flex align-items-center "  style="height: 80px; overflow: hidden; position: relative;"> 
+                    <a class="btn w-100 nameToForm" href="#"
+                    dataclient="{{$el -> name}}" datavalueid="{{$el -> id}}" data-bs-toggle="modal" data-bs-target="#taskModal"
+                   target="_blank">
+                    <i class="bi-clipboard-plus"></i></a>
+                  </div>                                         
+              </div> 
+          </div>
+        </div>
+
       </div>
 
     @endforeach
