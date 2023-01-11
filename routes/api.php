7ee87484d69017443698243b13c8c3d4247518cc
Mark <m6132@yandex.ru>
2023-01-11 10:30:32 +0300

    vera

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use DefStudio\Telegraph\Controllers\WebhookController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/telegraph/5941198915:AAFpQD_AvVJfiXjH6gaD3oBZgxbe06sTvyc/webhook', [WebhookController::class, 'handle'])->name('telegraph.webhook');
+Route::post('/telegraph/{token}/webhook', [WebhookController::class, 'handle'])->name('telegraph.webhook');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
