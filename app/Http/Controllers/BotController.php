23335e5b64a701acda898474976d5f147c03526c
Mark <m6132@yandex.ru>
2023-02-18 18:31:25 +0300

    post

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -22,7 +22,7 @@ $textMessage = urlencode($textMessage);
 $urlQuery = "https://api.telegram.org/bot". $token ."/sendMessage?chat_id=". $chat_id ."&text=" . $textMessage;
 
 $result = file_get_contents($urlQuery);
-
+ 
 /* //post
 $getQuery = array(
      "chat_id" 	=> 922556670,
