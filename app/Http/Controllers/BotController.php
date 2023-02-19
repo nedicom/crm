ad36b82629bc30476a0f01bb0efba38baa972436
Mark <m6132@yandex.ru>
2023-02-19 12:59:36 +0300

    data

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -27,11 +27,11 @@ class BotController extends Controller
 
             //post
 
-            /*
+            
             $data = file_get_contents('php://input');            
             $data = json_decode($data, true);
             file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
-            */
+            
 
 
             
