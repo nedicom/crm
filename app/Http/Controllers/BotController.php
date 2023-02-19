0d4e340558ecd4da176f13402c0450cdcd7222bd
Mark <m6132@yandex.ru>
2023-02-19 13:26:51 +0300

    text

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -31,8 +31,15 @@ class BotController extends Controller
             $data = json_decode($data, true);
             //file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
 
+            $getQuery = array(
+                "chat_id" 	=> $data['message']['chat']['id'],
+                //'reply_markup' => json_encode($keyboard),
+                "parse_mode" => "html",
+            );
+
             if(!empty($data['message']['text']) && $data['message']['text'] == '/start') {
                 $text = 'Давайте выберем юриста.';
+                $getQuery['text'] =  $text;
                 }
             else{
                 $text = 'test';
@@ -54,12 +61,7 @@ class BotController extends Controller
                     ]
                 ];*/
 
-            $getQuery = array(
-                "chat_id" 	=> $data['message']['chat']['id'],
-                "text"  	=> $text,
-                //'reply_markup' => json_encode($keyboard),
-                "parse_mode" => "html",
-            );
+
             $ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
