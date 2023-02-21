7780e4cede5ce58c985e2a597991aa332dc62805
Mark <m6132@yandex.ru>
2023-02-21 18:55:15 +0300

    keyboard

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -26,21 +26,22 @@ class BotController extends Controller
         */
 
             //post
-
-            $data = file_get_contents('php://input');            
+            $data = file_get_contents('php://input');
             $data = json_decode($data, true);
-            //file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
+            file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
             $message = $data['message']['text'];
 
             $keyboard['keyboard'][0] = ['в начало'];
-            $userkeyboard = [];
-            $tasklist[] = 'в начало'; $tasklist[] = 'в начало'; $tasklist[] = 'просроченные'; $tasklist[] = 'новые'; $tasklist[] = 'на сегодня';
-            $taskkeyboard['keyboard'][0] = ['в начало']; $taskkeyboard['keyboard'][1] = ['просроченные'];
-            $taskkeyboard['keyboard'][2] = ['новые']; $taskkeyboard['keyboard'][3] = ['на сегодня'];
+            
+            $tasklist = ['в начало', 'просроченные', 'новые', 'на сегодня'];
+            $taskkeyboard = ['keyboard'=>[['в начало','просроченные' ,'новые', 'на сегодня']]];
+            //$taskkeyboard['keyboard'][0] = ['в начало']; $taskkeyboard['keyboard'][1] = ['просроченные'];
+            //$taskkeyboard['keyboard'][2] = ['новые']; $taskkeyboard['keyboard'][3] = ['на сегодня'];
             $k = 1;
 
+            $userlist = [];
             foreach (User::all() as $lawyer) {
-                $userkeyboard[] = $lawyer->name;
+                $userlist[] = $lawyer->name;
                 $keyboard['keyboard'][$k] = [$lawyer->name];
                 $k++;
             }
@@ -56,7 +57,7 @@ class BotController extends Controller
                 $getQuery['reply_markup'] = json_encode($keyboard);                
                 }
 
-            elseif(!empty($message) && in_array($message, $userkeyboard)){
+            elseif(!empty($message) && in_array($message, $userlist)){
                 $text = 'Вы выбрали  - '.$message;
                 $getQuery['text'] =  $text;               
                 $getQuery['reply_markup'] = json_encode($taskkeyboard);
