a236d4e17c8bc34bdcb4213811cecf405340abaf
Mark <m6132@yandex.ru>
2023-02-21 19:00:11 +0300

    t3

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -34,7 +34,7 @@ class BotController extends Controller
             $keyboard['keyboard'][0] = ['в начало'];
             
             $tasklist = ['в начало', 'просроченные', 'новые', 'на сегодня'];
-            $taskkeyboard = ['keyboard'=>[[['в начало','просроченные' ,'новые', 'на сегодня']]]];
+            $taskkeyboard = ['keyboard'=>[[['в начало'],['просроченные'],['новые'], ['на сегодня']]]];
             //$taskkeyboard['keyboard'][0] = ['в начало']; $taskkeyboard['keyboard'][1] = ['просроченные'];
             //$taskkeyboard['keyboard'][2] = ['новые']; $taskkeyboard['keyboard'][3] = ['на сегодня'];
             $k = 1;
