9dc19517de5e090674565ffa6767640cbb493424
Mark <m6132@yandex.ru>
2023-02-25 11:31:25 +0300

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -89,7 +89,7 @@ class BotController extends Controller
                 $client = ClientsModel::where('id', $message)-> get();
                     if(count($client)){
                         $tasks = Tasks::where('clientid', $message)->where('status', '!=', 'выполнена')-> get();
-                        $textMessage = '<b>'.$client -> name.'</b>'."\n";
+                        $textMessage = '<b>'.print_r($client).'</b>'."\n";
                         if(count($tasks)){
                             foreach($tasks as $el){
                                 $textMessage .= '<b>'.$el -> name.'</b>'."\n";
