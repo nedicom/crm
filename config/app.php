f74207debc0c346e14faef41628409e4d5989b90
Denis Bedoyar <pulpirzvk@gmail.com>
2023-03-17 21:10:24 +0300

    Laravel Telescope

--- config/app.php
+++ config/app.php
@@ -104,6 +104,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
     ],
 
