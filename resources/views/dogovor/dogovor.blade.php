22a1d86c9dfe78c2877195ebcaaee897dd12ac8a
Denis Bedoyar <pulpirzvk@gmail.com>
2023-03-21 14:43:16 +0300

    Фикс ссылки на договор

--- resources/views/dogovor/dogovor.blade.php
+++ resources/views/dogovor/dogovor.blade.php
@@ -24,7 +24,7 @@
         <div class="card-header d-flex align-items-center">
            <span>
             @foreach($datalawyers as $ellawyer)
-            @if ($ellawyer -> id == $el -> lawyer_id)  
+            @if ($ellawyer -> id == $el -> lawyer_id)
             <img src="{{$ellawyer -> avatar}}" style="width: 40px; height:40px" class="rounded-circle">
             @endif
             @endforeach
@@ -34,11 +34,11 @@
 
         <div class="card-body">
           <div class="d-flex align-items-center justify-content-between">
-          
+
             <h2 class="px-2 text-truncate text-center" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" title="предмет" data-bs-content="{{$el -> subject}}"><strong>{{$el -> allstoimost}}</strong></h2>
 
             {{--<h5 class="px-2 text-truncate text-center" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" title="предоплата" data-bs-content="{{$el -> preduslugi}}"><strong>{{$el -> predoplata}}</strong></h5>
-              @php $percent = (($el -> allstoimost) - $avg)/$avg*100; $percent = round($percent, 2); @endphp              
+              @php $percent = (($el -> allstoimost) - $avg)/$avg*100; $percent = round($percent, 2); @endphp
               @if ($percent>0) <span style ="background-color:DodgerBlue;" class="position-absolute top-0 start-50 translate-middle badge rounded-pill" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Ваша сделка выгоднее других в среднем">+{{$percent}}%</span>
               @else <span style ="background-color:DimGray;" class="position-absolute top-0 start-50 translate-middle badge rounded-pill" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Ваша сделка дешевле других, но не всегда это плохо">{{$percent}}%</span>
               @endif  --}}
@@ -49,12 +49,12 @@
           <div class="d-flex align-items-center">
             <span class="px-2 text-truncate text-center">
                       @foreach($dataclients as $elclient)
-                      @if ($elclient -> id == $el -> client_id)  
+                      @if ($elclient -> id == $el -> client_id)
                       {{$elclient -> name}}
                       @endif
                       @endforeach
-            </span> 
-            @if($el -> url)<a href="public/{{$el -> url}}" class="btn btn-primary">скачать</a>@else <a href="#" class="btn btn-secondary disabled">скачать</a>@endif
+            </span>
+            @if($el -> url)<a href="/{{$el -> url}}" class="btn btn-primary">скачать</a>@else <a href="#" class="btn btn-secondary disabled">скачать</a>@endif
           </div>
 
           </div>
